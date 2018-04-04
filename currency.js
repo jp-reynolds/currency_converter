f3114170945fb8cc52ce0a9b620c4c766c3fe72c
jp-reynolds <jpreynolds215@gmail.com>
2018-04-04 00:15:55 -0600

    commit 2: added pesos

--- currency.js
+++ currency.js
@@ -2,44 +2,48 @@ $(document).ready(function() {
 
 	var currency_converted_endpoint = "http://data.fixer.io/api/latest?access_key=765658e565d5659c56b60b764d14a351&format=1";
 
-
-
-
-	$.get(currency_converted_endpoint, (response) => {
+	$.get(currency_converted_endpoint, function(response) {
+		//variable to access the 'rates' objects because this is where all the different conversion rates are at
 		var api = response.rates;
-		var dollars = $("#dollars").val();
+		// var dollars = $("#dollars").val();
 		//console.log(dollars);
 
-	$('#EUR').click( function (event) {
-		let eurConversionRate = api.EUR;
-
-		$('#eur').append('<p>' + dollars + ' dollars is equal to ' + (eurConversionRate * dollars * (1/1.227447)) + ' EUR');
-		// console.log(eurConversionRate * dollars * (1/1.227447));
+		$('#EUR').click( function (event) {
+			event.preventDefault();
+			let dollars = $("#dollars").val();
+		//I created a variable for each currencies specific conversion rate and labeled them the same in my html id's
+			let eurConversionRate = api.EUR;
+																		//when this toFixed(2) is added it makes the number into a string, not ideal...tried math.Round but couldn't get the precision to work
+			$('#eur').append('<p>' + '$' + dollars + ' is equal to ' + '€' + parseFloat((eurConversionRate * dollars * (1/1.227447)).toFixed(3)) + ' EUR');
 		});
 
-	$('#GBP').click( function (event) {
-		let gbpConversionRate = api.GBP;
+		$('#GBP').click( function (event) {
+			event.preventDefault();
+			let dollars = $("#dollars").val();
+			let gbpConversionRate = api.GBP;
+			$('#gbp').append('<p>' + '$' + dollars + ' is equal to ' + '£' + (gbpConversionRate * dollars * (1/1.227447)).toFixed(3) + ' GBP');
+		});
 
-		$('#gbp').append('<p>' + dollars + ' dollars is equal to ' + (gbpConversionRate * dollars * (1/1.227447)) + ' GBP');
-		// console.log(gbpConversionRate * dollars * (1/1.227447));
-	});
+		$('#CNY').click( function (event) {
+			event.preventDefault();
+			let dollars = $("#dollars").val();
+			let cnyConversionRate = api.CNY;
+			$('#cny').append('<p>' + '$' + dollars + ' is equal to ' + '¥' + (cnyConversionRate * dollars * (1/1.227447)).toFixed(3) + ' CNY');
+		});
 
-	$('#CNY').click( function (event) {
-		let cnyConversionRate = api.CNY;
+		$('#MXN').click( function (event) {
+			event.preventDefault();
+			let dollars = $("#dollars").val();
+			let mxnConversionRate = api.MXN;
+			$('#mxn').append('<p>' + '$' + dollars + ' is equal to ' + '¥' + (mxnConversionRate * dollars * (1/1.227447)).toFixed(3) + ' MXN');
+		});
 
-		$('#cny').append('<p>' + dollars + ' dollars is equal to ' + (cnyConversionRate * dollars * (1/1.227447)) + ' CNY');
-		// console.log(cnyConversionRate * dollars * (1/1.227447));
-	});
-
-	$('#JPY').click( function (event) {
-		let jpyConversionRate = api.JPY;
-
-		$('#jpy').append('<p>' + dollars + ' dollars is equal to ' + (jpyConversionRate * dollars * (1/1.227447)) + ' JPY');
-		// console.log(jpyConversionRate * dollars * (1/1.227447));
-	});
-
-
-
+		$('#JPY').click( function (event) {
+			event.preventDefault();
+			let dollars = $("#dollars").val();
+			let jpyConversionRate = api.JPY;
+			$('#jpy').append('<p>' + '$' + dollars + ' is equal to ' + '¥' + (jpyConversionRate * dollars * (1/1.227447)).toFixed(3) + ' JPY');
+		});
 	});
 
 
